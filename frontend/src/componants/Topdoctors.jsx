addafc3a445a46e86193306d92018472e20683b8
abrar <abrar@gmail.com>
2025-07-10 21:41:16 +0530

    change some festure

--- frontend/src/componants/Topdoctors.jsx
+++ frontend/src/componants/Topdoctors.jsx
@@ -15,9 +15,16 @@ const Topdoctors = () => {
                 <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className=' border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                     <img className='bg-[#d1ebe3]' src={item.image} alt="" />
                     <div className='p-4'>
+                        {item.available ? (
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                             <p className='w-2 h-2 bg-green-500 rounded-full '></p><p>Available</p>
                         </div>
+
+                        ) : (
+                             <div className='flex items-center gap-2 text-sm text-center text-red-500'>
+                            <p className='w-2 h-2 bg-red-500 rounded-full '></p><p>Unavailable</p>
+                        </div>
+                        )}
                         <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                         <p className='text-gray-600 text-sm'>{item.speciality} </p>
                     </div>
